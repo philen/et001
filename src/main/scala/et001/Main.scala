864330b59cbe0e12e414bbf24d5ecfbc4f06bf40
phillip <philen@gmail.com>
2022-05-20 07:00:08 -0400

    Refactor creation of LwjglApplicationConfiguration

--- src/main/scala/et001/Main.scala
+++ src/main/scala/et001/Main.scala
@@ -3,10 +3,11 @@ package et001
 import com.badlogic.gdx.backends.lwjgl.{LwjglApplication, LwjglApplicationConfiguration}
 
 object Main extends App {
-  val config = new LwjglApplicationConfiguration
-  config.title = "Engine Test 1"
-  config.width = 800
-  config.height = 600
-  config.forceExit = true
+  val config = new LwjglApplicationConfiguration {
+    title = "Engine Test 1"
+    width = 800
+    height = 600
+    forceExit = true
+  }
   new LwjglApplication(new EngineTest1, config)
 }
